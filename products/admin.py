decf00d182ffd08d7566310ab7928ab92949235e
Valeron <valerashankorenko@yandex.by>
2025-02-18 20:07:24 +0300

    Админка

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Subcategory
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'image')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'slug')
+    ordering = ('id',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'parent_category', 'slug', 'image')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'slug', 'parent_category__name')
+    ordering = ('parent_category', 'id')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'parent_subcategory', 'image_small')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name', 'parent_subcategory__name',
+                     'parent_subcategory__parent_category__name')
+    ordering = ('parent_subcategory', 'id')
+    empty_value_display = '-пусто-'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import User
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = (
+        'is_staff',
+        'is_superuser',
+        'groups',
+        'user_permissions'
+    )
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'date_joined_format',
+        'is_active'
+    )
+    list_display_links = (
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'date_joined_format'
+    )
+    search_fields = (
+        'email',
+        'first_name',
+        'last_name',
+    )
+    ordering = ('id',)
+    empty_value_display = '-пусто-'
+
+    def date_joined_format(self, obj):
+        return obj.date_joined.strftime(
+            '%d-%m-%Y %H:%M'
+        )
+
+    date_joined_format.short_description = 'Дата регистрации'
